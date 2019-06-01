aa35ce7d699a9a9a5c9951480fafa43cfc4128fd
Ivan Jaimes <ivan.jaimes@iver.mx>
2019-06-01 17:45:39 -0500

    Add goal test

--- lib/settings/goal.ex
+++ lib/settings/goal.ex
@@ -2,15 +2,39 @@ defmodule Balance.Goal do
   use Ecto.Schema
   import Ecto.Changeset
 
+  @levels ["A", "B", "C", "Cuauh"]
+
   schema "goal_settings" do
     field(:level, :string)
-    field(:goals, :integer, default: 0)
+    field(:goals, :integer)
     field(:team, :string, default: "all")
   end
 
   def changeset(struct, params) do
     struct
     |> cast(params, [:level, :goals, :team])
+    |> validate_required([:level, :goals])
+    |> validate_level()
+    |> validate_number(:goals, greater_than: 0)
   end
 
+  @doc """
+  Validate a level is belongs to the allowed levels
+
+  ## Examples
+
+     iex> change = Balance.Goal.changeset(%Balance.Goal{}, %{level: "B", goals: 7, team: "all"})
+     iex> Balance.Goal.validate_level(change)
+     #Ecto.Changeset<action: nil, changes: %{goals: 7, level: \"B\"}, errors: [], data: #Balance.Goal<>, valid?: true>
+
+  """
+  def validate_level(changeset) do
+    level = get_field(changeset, :level)
+
+    if level in @levels do
+      changeset
+    else
+      add_error(changeset, :level, "invalid level")
+    end
+  end
 end
---
+++ test/settings/goal_test.exs
@@ -0,0 +1,45 @@
+defmodule Balance.GoalTest do
+  use ExUnit.Case
+  doctest Balance.Goal
+
+  describe "GoalSettings" do
+    alias Balance.Goal
+    @valid_attrs %{level: "A", goals: 5, team: "all"}
+    @update_attrs %{level: "B", goals: 10, team: "rojo"}
+    @invalid_level %{level: "X", goals: 0, team: "all"}
+    @invalid_goal %{level: "C", team: "all"}
+
+    def save_goal(attrs, isValid) do
+      goal = Goal.changeset(%Goal{}, attrs)
+      # require IEx
+      # IEx.pry()
+      assert goal.valid? == isValid
+      Balance.Repo.insert(goal)
+    end
+
+    test "Save valid goal" do
+      goal = save_goal(@valid_attrs, true)
+      assert {:ok, %Goal{}} = goal
+    end
+
+    test "Save invalid level" do
+      goal = save_goal(@invalid_level, false)
+      assert {:error, result} = goal
+      assert result.errors[:level] == {"invalid level", []}
+    end
+
+    test "Save invalid goal" do
+      goal = save_goal(@invalid_goal, false)
+      assert {:error, result} = goal
+      assert result.errors[:goals] == {"can't be blank", [validation: :required]}
+    end
+
+    test "Update valid goal" do
+      goal = save_goal(@valid_attrs, true)
+      assert {:ok, goal2} = goal
+      goal2up = Goal.changeset(goal2, @update_attrs)
+      assert goal2up.valid? == true
+      assert {:ok, result} = Balance.Repo.update(goal2up)
+    end
+  end
+end

54a1dfc5617b61aebe94b7cf65275b3caddd5565
Ivan Jaimes <ivan.jaimes@iver.mx>
2019-06-10 10:22:08 -0500

    Test postgresql connection

--- mix.exs
+++ mix.exs
@@ -12,7 +12,7 @@ defmodule Balance.MixProject do
       aliases: aliases(),
       deps: deps(),
       test_coverage: [tool: ExCoveralls],
-      preferred_cli_env: ["coveralls": :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
+      preferred_cli_env: [coveralls: :test, "coveralls.detail": :test, "coveralls.post": :test, "coveralls.html": :test],
       escript: escript(),
 
       # Docs
